36adae05412cb76ea064782369650f377cc90f07
Float <eeykc10@nottingham.ac.uk>
2026-02-13 02:50:19 +0700

    Worksheet5: Doxygen setup

---
+++ Worksheet5/adder.h
@@ -0,0 +1,16 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/** @file
+ * This file contains the declarations of exported functions
+ */
+
+/** Adding function
+ * This function adds two numbers.
+ * @param a first number
+ * @param b second number
+ * @return sum of a and b
+ */
+int add(int a, int b);
+
+#endif
